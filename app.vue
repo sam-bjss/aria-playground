c69002eb8060d8b6483b9988a3f7d2f1264a42ea
Sam Widdowson <sam.widdowson@bjss.com>
2024-02-23 16:12:40 +0000

    General page styling

--- app.vue
+++ app.vue
@@ -5,7 +5,12 @@ useHead({
 </script>
 
 <template>
-    <h1 class="text-3xl">Default app.vue</h1>
-    <NuxtPage />
+    <h1
+        class="bg-gradient-to-bl from-emerald-400 from-20% to-white px-1 text-4xl"
+    >
+        Aria Playground.
+    </h1>
+    <div id="page-container" class="px-1 text-emerald-950">
+        <NuxtPage />
+    </div>
 </template>
-F
