88fb06d2c9be29d64105ccb5cf6a4801b4393c2f
Sam Widdowson <sam.widdowson@bjss.com>
2024-03-08 13:20:04 +0000

    Refactor nav list to make it easier to modify

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -16,13 +16,7 @@
                 >
                     On this page:
                 </h2>
-                <ul class="flex-grow list-none space-x-4">
-                    <NavLink to="/">/</NavLink>
-                    <NavLink to="/forms">/forms</NavLink>
-                    <NavLink to="/seating">/seating</NavLink>
-                    <NavLink to="/tabs">/tabs</NavLink>
-                    <NavLink to="/svg">/svg</NavLink>
-                </ul>
+                <TopNavList />
             </nav>
         </div>
     </header>
---
+++ components/layout/TopNavList.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="flex-grow list-none space-x-4">
+        <NavLink to="/">/</NavLink>
+        <NavLink to="/forms">/forms</NavLink>
+        <NavLink to="/seating">/seating</NavLink>
+        <NavLink to="/tabs">/tabs</NavLink>
+        <NavLink to="/svg">/svg</NavLink>
+        <NavLink to="/nav-with-flyout">/nav-with-flyout</NavLink>
+    </ul>
+</template>
