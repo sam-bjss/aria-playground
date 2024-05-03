4a681c263013e6a94df61b66bad75da9b19e9bae
Sam Widdowson <sam.widdowson@bjss.com>
2024-05-03 15:59:09 +0100

    Styling changes to allow Skip to Content accessibility in Safari

--- components/layout/SkipToContent.vue
+++ components/layout/SkipToContent.vue
@@ -1,10 +1,11 @@
 <template>
-    <a
-        href="#main-content"
-        :class="`z-100 fixed left-0 top-0 h-0 overflow-hidden
-                rounded-lg border-emerald-950 bg-white p-0
-                text-lg font-semibold text-emerald-950
-                focus:h-max focus:border-2 focus:p-4`"
-        >Skip to Content</a
+    <div
+        class="h-0 overflow-hidden border-0 border-emerald-950 bg-white p-0 focus-within:h-max focus-within:p-4"
     >
+        <a
+            href="#main-content"
+            class="bg-white text-2xl font-bold text-emerald-950"
+            >Skip to Content</a
+        >
+    </div>
 </template>
